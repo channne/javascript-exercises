bebdbece06d3747e04acdade43fbaabad2d6b5c1
channne <[cccx2303@gmail.com]>
2024-08-02 02:42:10 +0800

    finish arr and loops exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (!Number.isInteger(start) || !Number.isInteger(end)) {
+    return "ERROR";
+  }
+  if (start < 0 || end < 0) { 
+    return "ERROR";
+  }
 
+  let sum = 0;
+  if (start > end) {
+    let temp = start;
+    start = end;
+    end = temp;
+  }
+  return (end * (end+1) - (start-1) * start) / 2;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function round1dp(num) {
+  return (Math.round(num * 10) / 10);
+}
+
+const convertToCelsius = function(f) {
+  return round1dp((f - 32) * 5/9, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return round1dp(c * 9/5 + 32, 1);
 };
 
 // Do not edit below this line
